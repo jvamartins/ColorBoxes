730b701e69ef9cf95730b1142e593c84767f9e03
jvamartins <jvam90@outlook.com>
2021-06-06 12:09:53 -0300

    Adjustments

--- src/Box.js
+++ src/Box.js
@@ -1,37 +1,20 @@
 import React, { Component } from "react";
 import "./Box.css";
-import { getColor } from "./Helpers";
 
 class Box extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      color: null,
-    };
-    this.handleBoxClick = this.handleBoxClick.bind(this);
+    this.clickBox = this.clickBox.bind(this);
   }
 
-  handleBoxClick(e) {
-    this.boxClick();
+  clickBox(evt) {
+    this.props.clickBox(this.props.color, this.props.index);
   }
-
-  boxClick() {
-    let randomColor = getColor(this.props.colors);
-    while (randomColor === this.state.color) {
-      randomColor = getColor(this.props.colors);
-    }
-    this.setState({
-      color: randomColor,
-    });
-  }
-
   render() {
     let style = {
-      backgroundColor: this.state.color ? this.state.color : this.props.color,
+      backgroundColor: this.props.color,
     };
-    return (
-      <div onClick={this.handleBoxClick} className="Box" style={style}></div>
-    );
+    return <div onClick={this.clickBox} className="Box" style={style}></div>;
   }
 }
 
--- src/Container.js
+++ src/Container.js
@@ -4,6 +4,7 @@ import { getColor } from "./Helpers";
 
 class Container extends Component {
   static defaultProps = {
+    maxBox: 5,
     colors: [
       "red",
       "green",
@@ -18,21 +19,49 @@ class Container extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      boxes: this.fillBoxes(),
+    };
+    this.clickBox = this.clickBox.bind(this);
+  }
+
+  fillBoxes() {
+    let filledBoxes = [];
+    for (let i = 0; i < this.props.maxBox; i++) {
+      let color = this.fillColor();
+      filledBoxes.push(color);
+    }
+    return filledBoxes;
   }
 
   fillColor() {
     return getColor(this.props.colors);
   }
 
-  render() {
-    let boxes = [];
-
-    for (let i = 0; i < this.props.maxBox; i++) {
-      boxes.push(
-        <Box key={i} color={this.fillColor()} colors={this.props.colors} />
-      );
+  clickBox(currentColor, index) {
+    let randomColor = getColor(this.props.colors);
+    while (randomColor === currentColor) {
+      randomColor = getColor(this.props.colors);
     }
-    return <div>{boxes}</div>;
+    let newBoxes = this.state.boxes.map((box, i) => {
+      if (index === i) {
+        return randomColor;
+      }
+      return box;
+    });
+    this.setState({
+      boxes: newBoxes,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.boxes.map((b, i) => (
+          <Box key={i} index={i} color={b} clickBox={this.clickBox} />
+        ))}
+      </div>
+    );
   }
 }
 
